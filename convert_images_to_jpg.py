0a663fca499788b4c88081dac15002204da42570
Tim Stern <tim.stern@gmx.net>
2024-12-31 07:51:30 +0100

    Add files via upload

--- convert_images_to_jpg.py
+++ convert_images_to_jpg.py
@@ -5,11 +5,11 @@ Es sucht in einem angegebenen Verzeichnis (oder im aktuellen Verzeichnis) nach P
 Zusätzlich wird die zugehörige XML-Annotationsdatei im Pascal VOC-Format angepasst, um den neuen Dateinamen und Pfad widerzuspiegeln.
 
 Funktionsweise:
-1. Das Skript durchsucht das angegebene Verzeichnis (oder das aktuelle Arbeitsverzeichnis) nach PNG- und JPEG-Dateien.
-2. PNG-Bilder werden in das JPG-Format konvertiert, und die Originaldateien werden gelöscht.
+1. Das Skript durchsucht das angegebene Verzeichnis (oder das aktuelle Arbeitsverzeichnis) nach PNG-, WEBP-, AVIF- und JPEG-Dateien.
+2. PNG-, WEBP- und AVIF-Bilder werden in das JPG-Format konvertiert, und die Originaldateien werden gelöscht.
 3. JPEG-Bilder werden von `.jpeg` auf `.jpg` umbenannt.
 4. Jede betroffene XML-Datei im Pascal VOC-Format wird aktualisiert, um den neuen Bildnamen und Pfad widerzuspiegeln.
-5. Alle Klassennamen in den XML-Dateien werden in Kleinbuchstaben umgewandelt.
+5. Außerdem werden alle Klassennamen in den XML-Dateien in Kleinbuchstaben umgewandelt.
 
 Verwendung:
 1. Stelle sicher, dass die benötigten Bibliotheken installiert sind (z. B. Pillow für die Bildverarbeitung).
@@ -20,17 +20,19 @@ Verwendung:
      python convert_and_rename_images.py /pfad/zum/verzeichnis
      ```
 3. Ausgabe:
-   - Konvertiert alle PNG-Bilder in JPG und benennt JPEG-Bilder um.
+   - Konvertiert alle PNG-, WEBP- und AVIF-Bilder in JPG und benennt JPEG-Bilder um.
    - Aktualisiert die zugehörigen XML-Dateien, um den neuen Dateinamen und Pfad widerzuspiegeln.
 
 Hinweise:
-- Es werden nur PNG- und JPEG-Bilder verarbeitet.
+- Es werden PNG-, WEBP-, AVIF- und JPEG-Bilder verarbeitet.
 - Falls eine XML-Datei existiert, wird sie aktualisiert, um den neuen Dateinamen und Pfad widerzuspiegeln.
 - Klassennamen in den XML-Dateien werden in Kleinbuchstaben konvertiert.
 
 Abhängigkeiten:
 - Python 3.x
 - Pillow (Installierbar via `pip install pillow`)
+- pillow_avif
+- pillow_heif (für das WEBP-Format)
 - xml.etree.ElementTree (In Python integriert)
 - uuid (In Python integriert)
 
@@ -39,6 +41,8 @@ Abhängigkeiten:
 
 from sys import argv
 from pathlib import Path
+import pillow_avif
+from pillow_heif import register_heif_opener
 from PIL import Image
 import os
 import xml.etree.ElementTree as ET
@@ -50,43 +54,53 @@ if (len(argv) >= 2):
     image_path = argv[1]
 print('Folder: ' + image_path)
 
+register_heif_opener()
+
 
 # Update XML file if it exists:
-def updateXml(file_name):
-    folder = Path(file_name).parent
-    fn = Path(file_name).stem
-    xml_path = os.path.join(folder, fn + '.xml')
-    if os.path.isfile(xml_path):
-        xmlRoot = ET.parse(xml_path).getroot()
-        xmlRoot.find('filename').text = str(fn + '.jpg')
-        xmlRoot.find('path').text = str(file_name)
+def updateXml(origImgFile, newImgFile):
+    origXmlFile = origImgFile.with_suffix('.xml')
+    if origXmlFile.exists():
+        xmlRoot = ET.parse(origXmlFile).getroot()
+        xmlRoot.find('filename').text = str(newImgFile.name)
+        xmlRoot.find('path').text = str(newImgFile)
         for member in xmlRoot.findall('object'):
             member.find('name').text = member.find('name').text.lower()
+
+        newXmlFile = newImgFile.with_suffix('.xml')
         tree = ET.ElementTree(xmlRoot)
-        tree.write(xml_path)
+        tree.write(newXmlFile)
+        if (origXmlFile != newXmlFile):
+            os.remove(origXmlFile)
 
 
-# Converting PNG files:
-png_file_list = [path for path in Path(image_path).rglob('*.png')]
-num_files = len(png_file_list)
-if (num_files == 0):
-    print('No PNG images found in folder.')
-else:
-    print('Converting ' + str(num_files) + ' PNG images...')
+# Converting files:
+def convert(imagePath, ext):
+    fileList = [path for path in Path(imagePath).rglob(f"*.{ext}")]
+    numFiles = len(fileList)
+    if (numFiles == 0):
+        print(f"No {ext} images found in folder.")
+    else:
+        print(f"Converting {str(numFiles)} {ext} images...")
 
-    for file in png_file_list:
-        try:
-            origFile = file
-            im = Image.open(file)
-            rgb_im = im.convert('RGB')
-            new_name = file.with_suffix('.jpg')
-            while new_name.exists():
-                new_name = new_name.with_stem(str(uuid.uuid4().hex))
-            rgb_im.save(new_name)
-            os.remove(origFile)
-            updateXml(new_name)
-        except:
-            print(f'Error with file {file}')
+        for file in fileList:
+            try:
+                im = Image.open(file)
+                if not im.mode == 'RGB':
+                    im = im.convert('RGB')
+                new_name = file.with_suffix('.jpg')
+                while new_name.exists():
+                    new_name = new_name.with_stem(str(uuid.uuid4().hex))
+                im.save(new_name)
+                os.remove(file)
+                updateXml(file, new_name)
+            except Exception as e:
+                print(f"Error with file {file} =>", e)
+
+
+convert(image_path, 'png')
+convert(image_path, 'webp')
+convert(image_path, 'avif')
 
 
 # Renaming JPEG files:
@@ -102,7 +116,7 @@ else:
         while new_name.exists():
             new_name = new_name.with_stem(str(uuid.uuid4().hex))
         file.rename(new_name)
-        updateXml(new_name)
+        updateXml(file, new_name)
 
 
 print('Complete.')
